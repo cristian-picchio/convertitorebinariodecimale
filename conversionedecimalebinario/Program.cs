b6c432271879ba65b1d1b540ac048aa9a4563bfd
cristian-picchio <picchiocristian00@gmail.com>
2020-10-13 12:53:29 +0200

    Add project files.

---
+++ conversionedecimalebinario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace conversionedecimalebinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, resto;
+            Console.Write("inserisci il numero da convertire:");
+            num = int.Parse(Console.ReadLine());
+            string binario = "";
+            while (num > 0) ;
+            {
+                resto = num % 2;
+                num /= 2;
+                binario = Convert.ToString(resto) + binario;
+            }
+            Console.WriteLine($"il numero in binario è {binario}");
+            Console.ReadLine();
+        }
+
+    }
+}
